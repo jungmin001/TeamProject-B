566c7e65c553fcb0837a3e79dc21fcbb50704c28
jungmin001 <jungmin4556@naver.com>
2026-09-18 15:05:42 +0900

    Update

--- data_units.py
+++ data_units.py
@@ -2,15 +2,21 @@ import pandas as pd
 import numpy
 
 def load_dataset(F):
-    print("CSV 파일을 불러와 DataFrame으로 반환합니다.")
-    df = pd.read_csv(F)
-    return df
+    try:
+        print("LogTemp : Warning File load Complete! :D")
+        return pd.read_csv(F)
+    except FileNotFoundError:   # File path error
+        print("LogTemp : Error Failed to load CSV file!! ;(")
+        return None
+    except Exception:            # No file
+        print("LogTemp : Error Failed to load CSV file!! ;(")
+        return None
 
 def summarize_dataset(df,target_col=None):
-    df_summ = pd.DataFrame({"행/열 개수":df.shape,
-                        "컬럼명": df.columns,
-                        '결측치 수':df.isna().sum(),
-                        '타깃 분포':df[f'{target_col}'].value_counts(normalize=True)})
+    df_summ = pd.DataFrame({"행/열 개수" : df.shape,
+                            "컬럼명" : df.columns,
+                            '결측치 수' : df.isna().sum(),
+                            '타깃 분포' : df[f'{target_col}'].value_counts(normalize=True)})
     return df_summ
 
 
